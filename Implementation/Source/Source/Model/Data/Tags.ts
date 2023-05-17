5ced60c4a1cbb4aa1dbb70f5e208a5a283eb0fa9
ArseniyKruglov <arseniy.krugloff@gmail.com>
2023-05-17 15:30:36 +0300

    Added dots

--- Implementation/Source/Source/Model/Data/Tags.ts
+++ Implementation/Source/Source/Model/Data/Tags.ts
@@ -7,26 +7,26 @@ export const List: Tag.Interface[] =
 	{
 		'ID': 'Safe',
 		'ReadableName': 'Safe',
-		'Description': 'Has no side effects'
+		'Description': 'Has no side effects.'
 	},
 	{
 		'ID': 'Unsafe',
 		'ReadableName': 'Unsafe',
-		'Description': 'Might break your code'
+		'Description': 'Might break your code.'
 	},
 	{
 		'ID': 'Compression',
 		'ReadableName': 'Compression',
-		'Description': 'Optimizes code for better GZip or Brotli compression'
+		'Description': 'Optimizes code for better GZip or Brotli compression.'
 	},
 	{
 		'ID': 'SafeInsideOneFile',
 		'ReadableName': 'Safe inside one file',
-		'Description': 'Safe inside one file'
+		'Description': 'Safe inside one file.'
 	},
 	{
 		'ID': 'Configurable',
 		'ReadableName': 'Configurable',
-		'Description': 'Configurable'
+		'Description': 'Configurable.'
 	}
 ]
